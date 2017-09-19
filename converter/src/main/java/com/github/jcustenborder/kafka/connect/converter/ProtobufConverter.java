313126ffad0485561c099df2e993c2ac04955b8f
Jeremy Custenborder <jcustenborder@gmail.com>
2017-09-19 12:03:38 -0500

    Initial commit of structure.

---
+++ converter/src/main/java/com/github/jcustenborder/kafka/connect/converter/ProtobufConverter.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright © 2017 Jeremy Custenborder (jcustenborder@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jcustenborder.kafka.connect.converter;
+
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaAndValue;
+import org.apache.kafka.connect.storage.Converter;
+
+import java.util.Map;
+
+public class ProtobufConverter implements Converter {
+
+
+  public void configure(Map<String, ?> settings, boolean isKey) {
+
+  }
+
+  public byte[] fromConnectData(String topic, Schema schema, Object o) {
+    return new byte[0];
+  }
+
+  public SchemaAndValue toConnectData(String topic, byte[] bytes) {
+    return null;
+  }
+}
